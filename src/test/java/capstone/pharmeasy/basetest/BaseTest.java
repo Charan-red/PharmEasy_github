3ddfc89019332768b33611b0ed6636e8d913548e
sai.kumbham <sai.kumbham@in.ncs-i.com>
2024-04-11 18:24:41 +0530

    removed report

--- src/test/java/capstone/pharmeasy/basetest/BaseTest.java
+++ src/test/java/capstone/pharmeasy/basetest/BaseTest.java
@@ -1,36 +1,36 @@
-
-package capstone.pharmeasy.basetest;
-
-import org.openqa.selenium.WebDriver;
-import org.testng.ITestResult;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeTest;
-
-import capstone.pharmeasy.reports.*;
-//extends CustomReports
-public class BaseTest  {
-	public static WebDriver driver;
-
-//	@BeforeTest
-//	public void beforeTest() throws Exception {
-//		CustomReports.reportCreation();
 //
-//	}
-
-//	@AfterMethod
-//	public void afterMethod(ITestResult itr) throws Exception {
+//package capstone.pharmeasy.basetest;
 //
-////		if (!itr.isSuccess()) {
-////			TestUtility.capturescreenshot();
-////		}
-//		CustomReports.finishReportAfterTest(itr, null);
-//	}
-
-//	@AfterTest
-//	public void afterTest() {
-//		extent.flush();
-//	}
-
-
-}
+//import org.openqa.selenium.WebDriver;
+//import org.testng.ITestResult;
+//import org.testng.annotations.AfterMethod;
+//import org.testng.annotations.AfterTest;
+//import org.testng.annotations.BeforeTest;
+//
+//import capstone.pharmeasy.reports.*;
+////extends CustomReports
+//public class BaseTest  {
+//	public static WebDriver driver;
+//
+////	@BeforeTest
+////	public void beforeTest() throws Exception {
+////		CustomReports.reportCreation();
+////
+////	}
+//
+////	@AfterMethod
+////	public void afterMethod(ITestResult itr) throws Exception {
+////
+//////		if (!itr.isSuccess()) {
+//////			TestUtility.capturescreenshot();
+//////		}
+////		CustomReports.finishReportAfterTest(itr, null);
+////	}
+//
+////	@AfterTest
+////	public void afterTest() {
+////		extent.flush();
+////	}
+//
+//
+//}
